b69c79d1adce4a43285e52f532911d85cb3d2ada
rim.el-mallah <rimelmallah@Rims-MacBook-Pro.local>
2024-02-23 11:08:29 +0100

    some changes

--- src/train.py
+++ src/train.py
@@ -55,8 +55,8 @@ DQN = torch.nn.Sequential(nn.Linear(state_dim, nb_neurons),
 # DQN config
 config = {'nb_actions': env.action_space.n,
           'learning_rate': 0.001,
-          'gamma': 0.95,
-          'buffer_size': 10000,
+          'gamma': 0.99,
+          'buffer_size': 100000,
           'epsilon_min': 0.01,
           'epsilon_max': 1.,
           'epsilon_decay_period': 20000,
@@ -73,7 +73,9 @@ config = {'nb_actions': env.action_space.n,
 # ENJOY!
 class ProjectAgent:
     def __init__(self):
+        model = DQN
         device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.device = device
         self.nb_actions = config['nb_actions']
         self.gamma = config['gamma'] if 'gamma' in config.keys() else 0.95
         self.batch_size = config['batch_size'] if 'batch_size' in config.keys() else 100
@@ -84,7 +86,7 @@ class ProjectAgent:
         self.epsilon_stop = config['epsilon_decay_period'] if 'epsilon_decay_period' in config.keys() else 1000
         self.epsilon_delay = config['epsilon_delay_decay'] if 'epsilon_delay_decay' in config.keys() else 20
         self.epsilon_step = (self.epsilon_max-self.epsilon_min)/self.epsilon_stop
-        self.model = DQN 
+        self.model = model 
         self.target_model = deepcopy(self.model).to(device)
         self.criterion = config['criterion'] if 'criterion' in config.keys() else torch.nn.MSELoss()
         lr = config['learning_rate'] if 'learning_rate' in config.keys() else 0.001
@@ -93,6 +95,36 @@ class ProjectAgent:
         self.update_target_strategy = config['update_target_strategy'] if 'update_target_strategy' in config.keys() else 'replace'
         self.update_target_freq = config['update_target_freq'] if 'update_target_freq' in config.keys() else 20
         self.update_target_tau = config['update_target_tau'] if 'update_target_tau' in config.keys() else 0.005
+        self.monitoring_nb_trials = config['monitoring_nb_trials'] if 'monitoring_nb_trials' in config.keys() else 0
+
+    def MC_eval(self, env, nb_trials):   # NEW NEW NEW
+        MC_total_reward = []
+        MC_discounted_reward = []
+        for _ in range(nb_trials):
+            x,_ = env.reset()
+            done = False
+            trunc = False
+            total_reward = 0
+            discounted_reward = 0
+            step = 0
+            while not (done or trunc):
+                a = greedy_action(self.model, x)
+                y,r,done,trunc,_ = env.step(a)
+                x = y
+                total_reward += r
+                discounted_reward += self.gamma**step * r
+                step += 1
+            MC_total_reward.append(total_reward)
+            MC_discounted_reward.append(discounted_reward)
+        return np.mean(MC_discounted_reward), np.mean(MC_total_reward)
+    
+    def V_initial_state(self, env, nb_trials):   # NEW NEW NEW
+        with torch.no_grad():
+            for _ in range(nb_trials):
+                val = []
+                x,_ = env.reset()
+                val.append(self.model(torch.Tensor(x).unsqueeze(0).to(device)).max().item())
+        return np.mean(val)
     
     def gradient_step(self):
         if len(self.memory) > self.batch_size:
@@ -107,6 +139,9 @@ class ProjectAgent:
             
     def train(self, env, max_episode):
         episode_return = []
+        MC_avg_total_reward = []   # NEW NEW NEW
+        MC_avg_discounted_reward = []   # NEW NEW NEW
+        V_init_state = []   # NEW NEW NEW
         episode = 0
         episode_cum_reward = 0
         state, _ = env.reset()
@@ -138,48 +173,69 @@ class ProjectAgent:
                 model_state_dict = self.model.state_dict()
                 tau = self.update_target_tau
                 for key in model_state_dict:
-                    target_state_dict[key] = tau*model_state_dict[key] + (1-tau)*target_state_dict[key]
+                    target_state_dict[key] = tau*model_state_dict + (1-tau)*target_state_dict
                 self.target_model.load_state_dict(target_state_dict)
             # next transition
             step += 1
             if done or trunc:
                 episode += 1
+                # Monitoring
                 score = evaluate_HIV(agent=self, nb_episode=1)
-                print("Episode ", '{:3d}'.format(episode), 
-                      ", epsilon ", '{:6.2f}'.format(epsilon), 
-                      ", batch size ", '{:5d}'.format(len(self.memory)), 
-                      ", episode return ", '{:.4e}'.format(episode_cum_reward),
-                      ", score ", '{:.4e}'.format(score),
-                      sep='')
+                if self.monitoring_nb_trials>0:
+                    MC_dr, MC_tr = self.MC_eval(env, self.monitoring_nb_trials)    # NEW NEW NEW
+                    V0 = self.V_initial_state(env, self.monitoring_nb_trials)   # NEW NEW NEW
+                    MC_avg_total_reward.append(MC_tr)   # NEW NEW NEW
+                    MC_avg_discounted_reward.append(MC_dr)   # NEW NEW NEW
+                    V_init_state.append(V0)   # NEW NEW NEW
+                    episode_return.append(episode_cum_reward)   # NEW NEW NEW
+                    print("Episode ", '{:2d}'.format(episode), 
+                          ", epsilon ", '{:6.2f}'.format(epsilon), 
+                          ", batch size ", '{:4d}'.format(len(self.memory)), 
+                          ", ep return ", '{:4.1f}'.format(episode_cum_reward), 
+                          ", MC tot ", '{:6.2f}'.format(MC_tr),
+                          ", MC disc ", '{:6.2f}'.format(MC_dr),
+                          ", V0 ", '{:6.2f}'.format(V0),
+                          ", score ", '{:.3e}'.format(score),
+                          sep='')
+                else:
+                    episode_return.append(episode_cum_reward)
+                    print("Episode ", '{:2d}'.format(episode), 
+                          ", epsilon ", '{:6.2f}'.format(epsilon), 
+                          ", batch size ", '{:4d}'.format(len(self.memory)), 
+                          ", ep return ", '{:.3e}'.format(episode_cum_reward),
+                          ", score ", '{:.3e}'.format(score), 
+                          sep='')
+
+                
+                state, _ = env.reset()
+                episode_cum_reward = 0
                 if score > best_score:
                     best_score = score
-                    self.save("DQN_new.pt")
-                    print("Model saved")
-                state, _ = env.reset()
-                episode_return.append(episode_cum_reward)
-                episode_cum_reward = 0
+                    self.save("DQN_MC.pt")
+                    print("Best score, model saved")
             else:
                 state = next_state
-        return episode_return
-              
+        return episode_return, MC_avg_discounted_reward, MC_avg_total_reward, V_init_state
     
     def act(self, observation, use_random=False):
         if use_random:
             return env.action_space.sample()
         else:
-            a = greedy_action(self.model, observation)
+            s = observation
+            a = greedy_action(self.model, s)
             return a
 
     def save(self, path):
         torch.save(self.model.state_dict(), path)
 
     def load(self):
-        self.model.load_state_dict(torch.load("DQN_new.pt", map_location=torch.device('cpu')))
-
+        self.model.load_state_dict(torch.load("DQN_MC.pt"))
 
 # agent = ProjectAgent()
+# print(agent.device)
+# episode_return, MC_avg_discounted_reward, MC_avg_total_reward, V_init_state = agent.train(env, 400)
+# agent = ProjectAgent()
 # agent.load()
-# # evaluate the agent
 # print(evaluate_HIV(agent=agent, nb_episode=1))
 
 
